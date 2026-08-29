380655270b446215d9f5e2d88a873d8c5a8fda1b
Bibash Thapa <bibashthapa1432@gmail.com>
2026-08-29 22:17:47 +0545

    Add implementation of user input and output for name, age, and address

---
+++ basic/input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name,address;
+    int age;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<"Enter your address: ";
+    cin>>address;
+
+
+    cout<<"Hello "<<name<<"!!!!";
+    cout<<"\nYour age is "<<age;
+    cout<<"\nYour address is "<<address;
+    return 0;
+}
