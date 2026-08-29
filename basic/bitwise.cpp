333222787cb28985e5feb0330b375c811c1222f6
Bibash Thapa <bibashthapa1432@gmail.com>
2026-08-29 22:18:37 +0545

    Add implementation of bitwise operations in C++

---
+++ basic/bitwise.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=3;
+    cout<<"a & b = "<<(a & b)<<endl;  //bitwise AND
+    cout<<"a | b = "<<(a | b)<<endl;  //bitwise OR
+    cout<<"a ^ b = "<<(a ^ b)<<endl;  //bitwise XOR
+    cout<<"~a = "<<(~a)<<endl;        //bitwise NOT
+    cout<<"a << 1 = "<<(a << 1)<<endl; //left shift
+    cout<<"b >> 1 = "<<(b >> 1)<<endl; //right shift
+    return 0;
+}
